8e476b59651c5d5dfb4573adb554987ae9171836
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-03 17:42:20 +0400

    feature2 third commit

--- src/main/java/com/faang/test2/Main.java
+++ src/main/java/com/faang/test2/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("one");
         System.out.println("two");
+        System.out.println("three");
     }
 }
