c4c481e2b056e726586106421cd315efe7578c4a
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-03 18:18:41 +0400

    master second commit

--- src/main/java/com/faang/test/Main.java
+++
@@ -1,7 +0,0 @@
-package com.faang.test;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello from feature 1 branch");
-    }
-}
--- src/main/java/com/faang/test3/Main.java
+++
@@ -1,7 +0,0 @@
-package com.faang.test3;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Parvin");
-    }
-}
